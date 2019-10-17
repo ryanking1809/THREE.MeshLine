5f77307a23ef6ea14e5a969b56c0ed41804bbb2a
Ryan King <ryan.king1809@gmail.com>
2019-10-17 13:31:17 -0500

    export raycast function

--- src/THREE.MeshLine.js
+++ src/THREE.MeshLine.js
@@ -89,7 +89,7 @@
 
 	MeshLine.prototype.setVertices = function(vts, wcb) {
 		this._vertices = vts;
-		this.widthCallback = wcb ? wcb : this.widthCallback;
+		this.widthCallback = wcb || this.widthCallback;
 		this.positions = [];
 		this.counters = [];
 		for (var j = 0; j < vts.length; j++) {
@@ -105,7 +105,7 @@
 
 	MeshLine.prototype.setBufferArray = function(ba, wcb) {
 		this._bufferArray = ba;
-		this.widthCallback = wcb ? wcb : this.widthCallback;
+		this.widthCallback = wcb || this.widthCallback;
 		this.positions = [];
 		this.counters = [];
 		for (var j = 0; j < ba.length; j += 3) {
@@ -118,153 +118,147 @@
 		this.process();
 	};
 
-	MeshLine.prototype.raycast = (function() {
+	function MeshLineRaycast(raycaster, intersects) {
 		var inverseMatrix = new THREE.Matrix4();
 		var ray = new THREE.Ray();
 		var sphere = new THREE.Sphere();
+		var precision = raycaster.linePrecision;
+		var precisionSq = precision * precision;
+		var interRay = new THREE.Vector3();
+		var geometry = this.geometry;
+		// Checking boundingSphere distance to ray
 
-		return function raycast(raycaster, intersects) {
-			var precision = raycaster.linePrecision;
-			var precisionSq = precision * precision;
-			var interRay = new THREE.Vector3();
-			var geometry = this.geometry;
-			// Checking boundingSphere distance to ray
+		sphere.copy(geometry.boundingSphere);
+		sphere.applyMatrix4(this.matrixWorld);
 
-			sphere.copy(geometry.boundingSphere);
-			sphere.applyMatrix4(this.matrixWorld);
+		if (raycaster.ray.intersectSphere(sphere, interRay) === false) {
+			return;
+		}
 
-			if (raycaster.ray.intersectSphere(sphere, interRay) === false) {
-				return;
+		inverseMatrix.getInverse(this.matrixWorld);
+		ray.copy(raycaster.ray).applyMatrix4(inverseMatrix);
+
+		var vStart = new THREE.Vector3();
+		var vEnd = new THREE.Vector3();
+		var vStartPrevious = new THREE.Vector3();
+		var vEndPrevious = new THREE.Vector3();
+		var vStartNext = new THREE.Vector3();
+		var vEndNext = new THREE.Vector3();
+		var interSegment = new THREE.Vector3();
+		var step = this instanceof THREE.LineSegments ? 2 : 1;
+		var index = geometry.index;
+		var attributes = geometry.attributes;
+
+		if (index !== null) {
+			var indices = index.array;
+			var positions = attributes.position.array;
+			var previous = attributes.previous.array;
+			var next = attributes.next.array;
+			var sides = attributes.side.array;
+			var width = attributes.width.array;
+
+			for (var i = 0, l = indices.length - 1; i < l; i += 1) {
+				var a = indices[i];
+				var b = indices[i + 2]; // next index on same side
+
+				// get points before and after to calculate width direction
+				vStart.fromArray(positions, a * 3);
+				vEnd.fromArray(positions, b * 3);
+				vStartPrevious.fromArray(previous, a * 3);
+				vEndPrevious.fromArray(previous, b * 3);
+				vStartNext.fromArray(next, a * 3);
+				vEndNext.fromArray(next, b * 3);
+
+				// get width and side for each point
+				const widthStart = (width[i] * this.material.lineWidth) / 2;
+				const sideStart = sides[i];
+				const widthEnd = (width[i + 2] * this.material.lineWidth) / 2;
+				const sideEnd = sides[i + 2];
+
+				// https://discourse.threejs.org/t/move-vector3-along-normal-vector-math-help/10305
+				const lineDirectionStart = new THREE.Vector3();
+				const lineNormalStart = new THREE.Vector3();
+				// line between previous and next point
+				const lineDirectionSegmentStart = new THREE.Line3(
+					vStartPrevious,
+					vStartNext
+				);
+				// offset start so we have a direction to move the point
+				const vStartOffset = new THREE.Vector3(
+					vStart.x + sideStart,
+					vStart.y + sideStart,
+					vStart.z
+				);
+				lineDirectionSegmentStart.closestPointToPoint(
+					vStartOffset,
+					true,
+					lineDirectionStart
+				);
+				// get normal
+				lineNormalStart
+					.subVectors(vStartOffset, lineDirectionStart)
+					.normalize();
+				// move point along normal
+				vStart.add(lineNormalStart.multiplyScalar(widthStart));
+
+				// https://discourse.threejs.org/t/move-vector3-along-normal-vector-math-help/10305
+				const lineDirectionEnd = new THREE.Vector3();
+				const lineNormalEnd = new THREE.Vector3();
+				// line between previous and next point
+				const lineDirectionSegmentEnd = new THREE.Line3(
+					vEndPrevious,
+					vEndNext
+				);
+				// offset end so we have a direction to move the point
+				const vEndOffset = new THREE.Vector3(
+					vEnd.x + sideEnd,
+					vEnd.y + sideEnd,
+					vEnd.z
+				);
+				lineDirectionSegmentEnd.closestPointToPoint(
+					vEndOffset,
+					true,
+					lineDirectionEnd
+				);
+				// get normal
+				lineNormalEnd
+					.subVectors(vEndOffset, lineDirectionEnd)
+					.normalize();
+				// move point along normal
+				vEnd.add(lineNormalEnd.multiplyScalar(widthEnd));
+
+				var distSq = ray.distanceSqToSegment(
+					vStart,
+					vEnd,
+					interRay,
+					interSegment
+				);
+
+				if (distSq > precisionSq) continue;
+
+				interRay.applyMatrix4(this.matrixWorld); //Move back to world space for distance calculation
+
+				var distance = raycaster.ray.origin.distanceTo(interRay);
+
+				if (distance < raycaster.near || distance > raycaster.far)
+					continue;
+
+				intersects.push({
+					distance: distance,
+					// What do we want? intersection point on the ray or on the segment??
+					// point: raycaster.ray.at( distance ),
+					point: interSegment.clone().applyMatrix4(this.matrixWorld),
+					index: i,
+					face: null,
+					faceIndex: null,
+					object: this
+				});
+				// so we don't get multiple events
+				i = l;
 			}
-
-			inverseMatrix.getInverse(this.matrixWorld);
-			ray.copy(raycaster.ray).applyMatrix4(inverseMatrix);
-
-			var vStart = new THREE.Vector3();
-			var vEnd = new THREE.Vector3();
-			var vStartPrevious = new THREE.Vector3();
-			var vEndPrevious = new THREE.Vector3();
-			var vStartNext = new THREE.Vector3();
-			var vEndNext = new THREE.Vector3();
-			var interSegment = new THREE.Vector3();
-			var step = this instanceof THREE.LineSegments ? 2 : 1;
-			var index = geometry.index;
-			var attributes = geometry.attributes;
-
-			if (index !== null) {
-				var indices = index.array;
-				var positions = attributes.position.array;
-				var previous = attributes.previous.array;
-				var next = attributes.next.array;
-				var sides = attributes.side.array;
-				var width = attributes.width.array;
-
-				for (var i = 0, l = indices.length - 1; i < l; i += 1) {
-					var a = indices[i];
-					var b = indices[i + 2]; // next index on same side
-
-					// get points before and after to calculate width direction
-					vStart.fromArray(positions, a * 3);
-					vEnd.fromArray(positions, b * 3);
-					vStartPrevious.fromArray(previous, a * 3);
-					vEndPrevious.fromArray(previous, b * 3);
-					vStartNext.fromArray(next, a * 3);
-					vEndNext.fromArray(next, b * 3);
-
-					// get width and side for each point
-					const widthStart = (width[i] * this.material.lineWidth) / 2;
-					const sideStart = sides[i];
-					const widthEnd =
-						(width[i + 2] * this.material.lineWidth) / 2;
-					const sideEnd = sides[i + 2];
-
-					// https://discourse.threejs.org/t/move-vector3-along-normal-vector-math-help/10305
-					const lineDirectionStart = new THREE.Vector3();
-					const lineNormalStart = new THREE.Vector3();
-					// line between previous and next point
-					const lineDirectionSegmentStart = new THREE.Line3(
-						vStartPrevious,
-						vStartNext
-					);
-					// offset start so we have a direction to move the point
-					const vStartOffset = new THREE.Vector3(
-						vStart.x + sideStart,
-						vStart.y + sideStart,
-						vStart.z
-					);
-					lineDirectionSegmentStart.closestPointToPoint(
-						vStartOffset,
-						true,
-						lineDirectionStart
-					);
-					// get normal
-					lineNormalStart
-						.subVectors(vStartOffset, lineDirectionStart)
-						.normalize();
-					// move point along normal
-					vStart.add(lineNormalStart.multiplyScalar(widthStart));
-
-					// https://discourse.threejs.org/t/move-vector3-along-normal-vector-math-help/10305
-					const lineDirectionEnd = new THREE.Vector3();
-					const lineNormalEnd = new THREE.Vector3();
-					// line between previous and next point
-					const lineDirectionSegmentEnd = new THREE.Line3(
-						vEndPrevious,
-						vEndNext
-					);
-					// offset end so we have a direction to move the point
-					const vEndOffset = new THREE.Vector3(
-						vEnd.x + sideEnd,
-						vEnd.y + sideEnd,
-						vEnd.z
-					);
-					lineDirectionSegmentEnd.closestPointToPoint(
-						vEndOffset,
-						true,
-						lineDirectionEnd
-					);
-					// get normal
-					lineNormalEnd
-						.subVectors(vEndOffset, lineDirectionEnd)
-						.normalize();
-					// move point along normal
-					vEnd.add(lineNormalEnd.multiplyScalar(widthEnd));
-
-					var distSq = ray.distanceSqToSegment(
-						vStart,
-						vEnd,
-						interRay,
-						interSegment
-					);
-
-					if (distSq > precisionSq) continue;
-
-					interRay.applyMatrix4(this.matrixWorld); //Move back to world space for distance calculation
-
-					var distance = raycaster.ray.origin.distanceTo(interRay);
-
-					if (distance < raycaster.near || distance > raycaster.far)
-						continue;
-
-					intersects.push({
-						distance: distance,
-						// What do we want? intersection point on the ray or on the segment??
-						// point: raycaster.ray.at( distance ),
-						point: interSegment
-							.clone()
-							.applyMatrix4(this.matrixWorld),
-						index: i,
-						face: null,
-						faceIndex: null,
-						object: this
-					});
-					// so we don't get multiple events
-					i = l;
-				}
-			}
-		};
-	})();
-
+		}
+	}
+	MeshLine.prototype.raycast = MeshLineRaycast;
 	MeshLine.prototype.compareV3 = function(a, b) {
 		var aa = a * 6;
 		var ab = b * 6;
@@ -826,13 +820,16 @@
 		if (typeof module !== "undefined" && module.exports) {
 			exports = module.exports = {
 				MeshLine: MeshLine,
-				MeshLineMaterial: MeshLineMaterial
+				MeshLineMaterial: MeshLineMaterial,
+				MeshLineRaycast: MeshLineRaycast
 			};
 		}
 		exports.MeshLine = MeshLine;
 		exports.MeshLineMaterial = MeshLineMaterial;
+		exports.MeshLineRaycast = MeshLineRaycast;
 	} else {
 		root.MeshLine = MeshLine;
 		root.MeshLineMaterial = MeshLineMaterial;
+		root.MeshLineRaycast = MeshLineRaycast;
 	}
 }.call(this));
